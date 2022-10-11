ef4e8b25eadb6693385832ca4102cf990e5b4a84
Yigit Ocak <yigitocak@Yigits-MBP.lan>
2022-10-11 12:32:57 -0500

    CSS/HTML and JS files are updated

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const currentDay = moment().format('dddd, MMMM Do');
+$('#currentDay').text(currentDay);
+
+const currentHour = parseInt(moment().format("H"));
+$('#currentHour').text(currentHour);
+
+const timeList = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
+
+for (let i = 0; i < timeList.length; i++) {
+
+    if (currentHour === 12) {
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
